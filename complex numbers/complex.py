be6c3327cd5208219717dd6ff1df5083af179804
Francesco Leogrande <78494647+eazyclap@users.noreply.github.com>
2022-12-14 14:24:57 +0000

    complex.py

---
+++ complex numbers/complex.py
@@ -0,0 +1,22 @@
+"""Complex numbers"""
+
+import matplotlib.pyplot as plt
+
+class Complex:
+    """Complex number data type"""
+    def __init__(self, real: float, imm: float, name: str = "z") -> None:
+        self.real = real
+        self.imm = imm
+        self.name = name
+
+    def plot(self, color: str = "blue"):
+        "plot the complex number"
+        if self.imm >= 0:
+            label = f"{self.name} = {self.real} + {self.imm}i"
+        else:
+            lable = f"{self.name} = {self.real} - {-1 * self.imm}i"
+        plt.plot(self.real, self.imm, c=color, label=label)
+        plt.axvline(x=0, c="black")
+        plt.axhline(y=0, c="black")
+        plt.legend()
+        plt.show()
